f5ef7622e3c4fc5f88037785de04687a90cc0ad3
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-03-03 17:32:44 -0500

    hasta clase 11 ciclo de vida programa

---
+++ 2.hola-mundo/app.js
@@ -0,0 +1,9 @@
+const nombre = 'fernado';
+console.log(nombre);
+
+const nombre2 = 'Santiago';
+
+console.log(nombre, nombre2);
+
+/* El primer hola mundo */
+console.log('Hola mundo');
---
+++ 2.hola-mundo/app2.js
@@ -0,0 +1,9 @@
+/* 
+ *Basico
+ */
+
+const saludar = (nombre) => {
+    return `Saludos ${nombre} un gusto tenerte en el curso de node`;
+}
+
+console.log(saludar('Santiago'));
