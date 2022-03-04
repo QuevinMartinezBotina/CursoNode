21d65cfc5fd9b94defcd02d359b97f11ca14da8d
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-03-04 00:57:18 -0500

    hasta clase 17

--- 2.hola-mundo/app.js
+++ 2.hola-mundo/app.js
@@ -6,4 +6,4 @@ const nombre2 = 'Santiago';
 console.log(nombre, nombre2);
 
 /* El primer hola mundo */
-console.log('Hola mundo');
+console.log('Hola mundo hermoso');
---
+++ 3.const-vs-let-vs-var/template-string.js
@@ -0,0 +1,9 @@
+const nombre = 'Dead Pool';
+const real = 'Wade Wilson';
+
+const normal = nombre +' - '+ real;
+const template  = `${nombre} ${real}`;
+
+console.log(normal);
+console.log(template);
+
